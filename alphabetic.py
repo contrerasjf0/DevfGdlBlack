63bbb74f1c8817ea38490ffa17d33d15831c2dac
arturoar <a.reynoso.ibarra@gmail.com>
2017-09-22 21:09:50 -0500

    Alphabetic sorting working

--- alphabetic.py
+++ alphabetic.py
@@ -3,9 +3,11 @@ Obtains a list of products.
 Each product
 '''
 
+class alphabetic:
+	def sort(products):
+		for i in range(len(products)-1):
+			for j in range(len(products)-1):
+				if products[j].nombre > products[j+1].nombre:
+					products[j+1], products[j] = products[j], products[j+1]
 
-def alphabetic(products):
-
-	for i in range(len(products)-1):
-		if products[i].name > products[i+1].name:
-			products[i+1], products[i] = products[i], products[i+1]
+		return products
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from sortPrice import SortPrice
+from alphabetic import alphabetic
 
 productList = [Product('producto5', 19.50, 'A'), Product('producto4', 2.00, 'A'), Product('producto3', 4.67, 'A'), Product('producto2', 23.45, 'A'), Product('producto1', 45.00, 'A')]
 
@@ -11,6 +12,10 @@ print('Order by price')
 
 for product in sortList1:
     print(product.nombre, product.costo)
+
+sortList2 = alphabetic.sort(productList)
+for product in sortList1:
+	print(sortList2)
 
 
 

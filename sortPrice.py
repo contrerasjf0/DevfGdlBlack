4d4f869913b853acab5d85140642f12803c09233
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2017-09-22 21:12:28 -0500

    Merge ultimo comit

--- sortPrice.py
+++ sortPrice.py
@@ -2,11 +2,11 @@ class SortPrice:
 
     def price(self, productListOrigin):
         productList = productListOrigin
-        for i in range(1,len(productList)):
-            for j in range(1,len(productList)-i):
-                if(productList[j+1].costo < productList[j].costo):
+        for i in range(0,len(productList)):
+            for j in range(0,len(productList)-i):
+                if(productList[j].costo < productList[j-1].costo):
                     aux=productList[j].costo;
-                    productList[j].costo=productList[j+1].costo;
-                    productList[j+1].costo=aux;
+                    productList[j].costo=productList[j-1].costo;
+                    productList[j-1].costo=aux;
 
         return productList;
